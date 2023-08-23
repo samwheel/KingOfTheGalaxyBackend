bc265fc301e12f02c1460630ad1ee3c125aba2bc
Samuel Wheelwright <109480283+samwheel@users.noreply.github.com>
2023-08-23 15:08:21 -0600

    Add files via upload

---
+++ src/main/java/com/wheelwright/KingOfTheGalaxy/KingOfTheGalaxyApplication.java
@@ -0,0 +1,11 @@
+package com.wheelwright.KingOfTheGalaxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingOfTheGalaxyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KingOfTheGalaxyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wheelwright/KingOfTheGalaxy/KingOfTheGalaxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wheelwright.KingOfTheGalaxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingOfTheGalaxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
